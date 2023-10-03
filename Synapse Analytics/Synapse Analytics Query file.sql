44a28cb8a06a777fa09c72823176a6af6ff0a325
Pakhi <93408790+Bloom-98@users.noreply.github.com>
2023-10-04 01:47:23 +0530

    Update Synapse Analytics Query file.sql

--- Synapse Analytics/Synapse Analytics Query file.sql
+++ Synapse Analytics/Synapse Analytics Query file.sql
@@ -1,17 +1,27 @@
+SELECT * FROM athletes
+SELECT * FROM coaches
+SELECT * FROM entriesgender
+SELECT * FROM medals
+SELECT * FROM teams
+
 -- count the number of atheletes from each country
+
 SELECT Country, COUNT(*) as TotalAthletes
 FROM athletes 
 GROUP by Country
 ORDER BY TotalAthletes DESC;
 
-select * from entriesgender
+
 
 -- calculate the total medals won by each country
+  
 SELECT Team_Country, SUM(Gold) as Gold, SUM(Silver) as Silver,
 SUM(Bronze) as Bronze
 FROM medals
 GROUP BY Team_Country
 
+
+  
 -- calculate the average number of entries by gender for each discipline
 
 SELECT Discipline, (CAST(Female AS FLOAT)/CAST(Total AS FLOAT)) as AvgFemale, 
